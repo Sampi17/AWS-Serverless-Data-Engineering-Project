00e47467e91c8224b9969d5d74b307670276ed94
Samukelo Mnguni <princemagubelo@gmail.com>
2025-07-10 15:25:01 +0200

    changes to local file data_extraction.sh

--- data_ingestion/Scripts/data_extraction.sh
+++ data_ingestion/Scripts/data_extraction.sh
@@ -3,16 +3,16 @@
 current_dir=$(pwd)
 
 # downloading the dataset from kaggle using an api 
-python "$current_dir/Script/ingestion.py"
+python "$current_dir/Scripts/ingestion.py"
 
 #unzip file
 echo "unziping zip dataset"
 unzip -O "$current_dir/dataset/yellow_tripdata_2016-01.csv" -d data/
 
 # partitions the data for easy uploads
-python "$current_dir/Script/partition.py"
+python "$current_dir/Scripts/partition.py"
 
 # upload to S3 bucket
-python "$current_dir/Script/upload_to_s3.py"
+python "$current_dir/Scripts/upload_to_s3.py"
 
 
